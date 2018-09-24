e8ba22b8f907932565d34c13ffeacbaafa24fb7c
apujadas <andres.pujadas@mercadolibre.com>
2018-09-24 20:26:19 -0300

    recibo parametro en el POST

--- api/src/src/main/java/org/ungs/gorgory/controller/CodeEvaluationController.java
+++ api/src/src/main/java/org/ungs/gorgory/controller/CodeEvaluationController.java
@@ -4,12 +4,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.ungs.gorgory.bean.CodeEvaluationPayload;
 
 @RestController
 public class CodeEvaluationController {
 
     @PostMapping("/test/{guid}")
-    public void test(@PathVariable String guid, @RequestBody Object input) {
+    public void test(@PathVariable String guid, @RequestBody CodeEvaluationPayload input) {
         //llamar al service para ejecutar en un ejercicio en particular.
     }
 
