ecf5f2a0d021fb39831e067c738b7593f7203862
Alejandro Herrera <alejandro.herrera@mercadolibre.com>
2018-09-26 20:56:02 -0300

    agrego componentes de material ui

--- client/src/containers/App/index.jsx
+++ client/src/containers/App/index.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import TextField from '@material-ui/core/TextField';
+import Select from '@material-ui/core/Select';
+import { FormControl, InputLabel, Button } from '@material-ui/core';
+import MenuItem from '@material-ui/core/MenuItem';
 import { store } from '../../store';
 
-const App = () => (
-  <Provider store={store}>
-    <div>hola</div>
-  </Provider>
-);
+class App extends Component {
+  state = { age: 2 };
+  render() {
+    return (
+      <Provider store={store}>
+        <div>
+          <form>
+            <TextField />
+            <FormControl>
+              <InputLabel htmlFor="age-simple">Lenguaje</InputLabel>
+              <Select
+                value={this.state.age}
+                onChange={this.handleChange}
+                inputProps={{
+                  name: 'age',
+                  id: 'age-simple',
+                }}
+              >
+                <MenuItem value="">Ninguno</MenuItem>
+                <MenuItem value="java">Java</MenuItem>
+                <MenuItem value="python">Python</MenuItem>
+              </Select>
+            </FormControl>
+            <Button onClick={this.handleClick}>Boton</Button>
+          </form>
+        </div>
+      </Provider>
+    );
+  }
+}
 
 export default App;
