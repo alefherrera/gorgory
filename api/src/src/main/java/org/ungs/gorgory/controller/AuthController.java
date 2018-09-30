5af2850650626f85ad0f7dd413db4dcb38e992aa
Alejandro Herrera <alejandro.herrera@mercadolibre.com>
2018-09-30 18:48:17 -0300

    implementacion jwt

---
+++ api/src/src/main/java/org/ungs/gorgory/controller/AuthController.java
@@ -0,0 +1,54 @@
+package org.ungs.gorgory.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.ungs.gorgory.bean.JwtAuthenticationResponse;
+import org.ungs.gorgory.bean.LoginPayload;
+import org.ungs.gorgory.bean.SignUpPayload;
+import org.ungs.gorgory.security.JwtTokenProvider;
+import org.ungs.gorgory.service.UserService;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenProvider tokenProvider;
+
+    public AuthController(UserService userService, AuthenticationManager authenticationManager, JwtTokenProvider tokenProvider) {
+        this.userService = userService;
+        this.authenticationManager = authenticationManager;
+        this.tokenProvider = tokenProvider;
+    }
+
+    @PostMapping("/signup")
+    public void signUp(@RequestBody SignUpPayload payload) {
+        userService.signUp(payload.getUsername(), payload.getPassword());
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtAuthenticationResponse> login(@RequestBody LoginPayload payload) {
+
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        payload.getUsername(),
+                        payload.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = tokenProvider.generateToken(authentication);
+        return ResponseEntity.ok(new JwtAuthenticationResponse(jwt));
+    }
+
+}
--- api/src/src/main/java/org/ungs/gorgory/repository/UserRepository.java
+++ api/src/src/main/java/org/ungs/gorgory/repository/UserRepository.java
@@ -4,5 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.ungs.gorgory.model.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+
+    Optional<User> findByUsernameOrEmail(String username, String email);
+
+    Optional<User> findById(Long id);
+
 }
