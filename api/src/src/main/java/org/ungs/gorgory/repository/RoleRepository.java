044ffd1739a940a903ff5f7bb25674762deec49d
Alejandro Herrera <alejandro.herrera@mercadolibre.com>
2018-10-01 13:35:01 -0300

    agrego dependencia a data-rest

--- api/src/src/main/java/org/ungs/gorgory/repository/RoleRepository.java
+++ api/src/src/main/java/org/ungs/gorgory/repository/RoleRepository.java
@@ -2,7 +2,9 @@ package org.ungs.gorgory.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 import org.ungs.gorgory.model.Role;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long>, JpaSpecificationExecutor<Role> {
 }
--- api/src/src/main/java/org/ungs/gorgory/repository/UserRepository.java
+++ api/src/src/main/java/org/ungs/gorgory/repository/UserRepository.java
@@ -2,10 +2,12 @@ package org.ungs.gorgory.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 import org.ungs.gorgory.model.User;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
 
     Optional<User> findByUsernameOrEmail(String username, String email);
