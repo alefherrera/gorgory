ecf7782a5c1ecf023e991ac3cc16f94c84616534
Alejandro Herrera <alejandro.herrera@mercadolibre.com>
2018-10-06 20:57:57 -0300

    creo reducer para la sesion

---
+++ client/src/reducers/session.js
@@ -0,0 +1,34 @@
+import typeToReducer from 'type-to-reducer';
+import { FULFILLED } from 'redux-promise-middleware';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { LOGIN, LOGOUT } from '../constants';
+
+const persistConfig = {
+  key: 'login',
+  storage,
+};
+
+const initialState = {
+  username: '',
+  permissions: [],
+};
+
+const replaceState = () => ({
+  [FULFILLED]: (state, { payload }) => ({
+    ...state,
+    ...payload,
+  }),
+});
+
+export default persistReducer(
+  persistConfig,
+  typeToReducer(
+    {
+      [LOGIN]: replaceState(),
+      // [REFRESH_TOKEN]: replaceState(),
+      [LOGOUT]: () => initialState,
+    },
+    initialState,
+  ),
+);
