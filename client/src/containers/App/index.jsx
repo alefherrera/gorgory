b4b64e3a9009fcd42ab12c3bca33a71463b743b9
Alejandro Herrera <alejandro.herrera@mercadolibre.com>
2018-10-06 22:27:48 -0300

    agrego rooteo y estilos

--- client/src/containers/App/index.jsx
+++ client/src/containers/App/index.jsx
@@ -1,19 +1,47 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-
-import { store } from '../../store';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { ConnectedRouter } from 'react-router-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import Route from 'react-router/Route';
+import { store, persistor } from '../../store';
+import history from '../../history';
 import AppHeader from '../AppHeader';
 import Body from '../Body';
 // import CodeEditor from '../CodeEditor';
 import Login from '../Login';
 
+const theme = createMuiTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      light: '#FFCCBC',
+      main: '#FF5722',
+      dark: '#E64A19',
+      contrastText: '#ffffff',
+    },
+    secondary: {
+      light: '#F5F5F5',
+      main: '#9E9E9E',
+      dark: '#616161',
+      contrastText: '#000000',
+    },
+  },
+});
+
 const App = () => (
   <Provider store={store}>
-    <AppHeader>
-      <Body>
-        <Login />
-      </Body>
-    </AppHeader>
+    <PersistGate persistor={persistor}>
+      <MuiThemeProvider theme={theme}>
+        <ConnectedRouter history={history}>
+          <AppHeader>
+            <Body>
+              <Route path="/login" component={Login} />
+            </Body>
+          </AppHeader>
+        </ConnectedRouter>
+      </MuiThemeProvider>
+    </PersistGate>
   </Provider>
 );
 
