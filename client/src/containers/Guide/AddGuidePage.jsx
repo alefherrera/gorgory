cdfae1942e196d57e66b465df1e02c292c648e29
Alejandro Herrera <alejandro.herrera@mercadolibre.com>
2018-10-17 09:27:39 -0300

    agrego arbol de ejercicio, caso de pruebas y argumentos

--- client/src/containers/Guide/AddGuidePage.jsx
+++ client/src/containers/Guide/AddGuidePage.jsx
@@ -1,16 +1,92 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Field, reduxForm } from 'redux-form';
+import { Field, FieldArray, reduxForm } from 'redux-form';
 import { MenuItem } from '@material-ui/core';
 import FormComponent from '../../components/FormComponent';
 import TextFieldWrapper from '../../components/TextFieldWrapper';
 import SelectWrapper from '../../components/SelectWrapper';
 
 class AddGuidePage extends Component {
-  state = {};
-
   handleSubmit = () => {};
+
+  renderExercise = ({ fields }) => (
+    <ul>
+      <li>
+        <button type="button" onClick={() => fields.push({})}>
+          Agregar ejercicio
+        </button>
+      </li>
+      {fields.map((exercise, index) => (
+        <li key={index}>
+          <button type="button" onClick={() => fields.remove(index)}>
+            Eliminar
+          </button>
+          <h4>{`Ejercicio ${index + 1}`}</h4>
+          <Field
+            name={`${exercise}.firstName`}
+            type="text"
+            component={TextFieldWrapper}
+            label="Enunciado"
+          />
+          <FieldArray name={`${exercise}.testCases`} component={this.renderTestCase} />
+        </li>
+      ))}
+    </ul>
+  );
+
+  renderTestCase = ({ fields }) => (
+    <ul>
+      <li>
+        <button type="button" onClick={() => fields.push({})}>
+          Agregar caso de prueba
+        </button>
+      </li>
+      {fields.map((testCase, index) => (
+        <li key={index}>
+          <button type="button" onClick={() => fields.remove(index)}>
+            Eliminar
+          </button>
+          <Field
+            name={`${testCase}.expected`}
+            type="text"
+            component={TextFieldWrapper}
+            label="Esperado"
+          />
+          <Field
+            name={`${testCase}.signature`}
+            type="text"
+            component={TextFieldWrapper}
+            label="Firma"
+          />
+          <FieldArray name={`${testCase}.arguments`} component={this.renderArgument} />
+        </li>
+      ))}
+    </ul>
+  );
+
+  renderArgument = ({ fields }) => (
+    <ul>
+      <li>
+        <button type="button" onClick={() => fields.push({})}>
+          Agregar argumento
+        </button>
+      </li>
+      {fields.map((argument, index) => (
+        <li key={index}>
+          <button type="button" onClick={() => fields.remove(index)}>
+            Eliminar
+          </button>
+          <Field
+            name={`${argument}.value`}
+            type="text"
+            component={TextFieldWrapper}
+            label="Valor"
+          />
+        </li>
+      ))}
+    </ul>
+  );
 
   render() {
     return (
@@ -20,10 +96,11 @@ class AddGuidePage extends Component {
         onSubmit={this.props.handleSubmit(this.handleSubmit)}
       >
         <Field name="lang" label="Lenguaje" component={SelectWrapper}>
-          <MenuItem value="java">Java</MenuItem>
-          <MenuItem value="python">Python</MenuItem>
+          <MenuItem value="JAVA">Java</MenuItem>
+          <MenuItem value="PYTHON">Python</MenuItem>
         </Field>
-        <Field name="code" label="Codigo" component={TextFieldWrapper} />
+        <Field name="name" label="Nombre" component={TextFieldWrapper} />
+        <FieldArray name="exercises" component={this.renderExercise} />
       </FormComponent>
     );
   }
