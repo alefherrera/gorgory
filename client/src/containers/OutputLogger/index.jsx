6c2d1f941cc067c68e3f10f6876fdb11c413af50
apujadas <andres.pujadas@mercadolibre.com>
2018-10-17 20:21:07 -0300

    tabla para mostrado de resultados de los testcases

--- client/src/containers/OutputLogger/index.jsx
+++ client/src/containers/OutputLogger/index.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import ResultTable from '../../components/ResultTable'
 import { outputSelector } from '../../selectors/resolution';
 
-const OutputLogger = ({ output }) => <div>{JSON.stringify(output)}</div>;
+
+class OutputLogger extends Component {
+  render() {
+    return (
+      <ResultTable output={this.props.output} />
+    );
+  }
+}
 
 OutputLogger.propTypes = {
   output: PropTypes.object,
@@ -11,7 +19,68 @@ OutputLogger.propTypes = {
 
 export default connect(
   state => ({
-    output: outputSelector(state),
+    output: mockResults,
   }),
   null,
 )(OutputLogger);
+
+const mockResults = [{
+  testCase: { expected: 1234 },
+  output: 1234,
+  passed: true
+},
+{
+  testCase: { expected: 2345 },
+  output: 23456,
+  passed: false
+},
+{
+  testCase: { expected: "Hello world" },
+  output: "Hola mundo",
+  passed: false
+},
+{
+  testCase: { expected: "Caca" },
+  output: "Caca",
+  passed: true
+},
+{
+  testCase: { expected: "Caca" },
+  output: "Caca",
+  passed: true
+},
+{
+  testCase: { expected: "Caca" },
+  output: "Caca",
+  passed: true
+},
+{
+  testCase: { expected: "Caca" },
+  output: "Caca",
+  passed: true
+},
+{
+  testCase: { expected: "Caca" },
+  output: "Caca",
+  passed: true
+},
+{
+  testCase: { expected: "Caca" },
+  output: "Caca",
+  passed: true
+},
+{
+  testCase: { expected: "Caca" },
+  output: "Caca",
+  passed: true
+},
+{
+  testCase: { expected: "Caca" },
+  output: "Caca",
+  passed: true
+},
+{
+  testCase: { expected: "Caca" },
+  output: "Caca",
+  passed: true
+}]
