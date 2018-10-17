6942a78e2cd341be2a730c26956b1750f6e597b3
apujadas <andres.pujadas@mercadolibre.com>
2018-10-17 20:31:11 -0300

    delete mock results

--- client/src/containers/OutputLogger/index.jsx
+++ client/src/containers/OutputLogger/index.jsx
@@ -19,68 +19,7 @@ OutputLogger.propTypes = {
 
 export default connect(
   state => ({
-    output: mockResults,
+    output: outputSelector(state),
   }),
   null,
 )(OutputLogger);
-
-const mockResults = [{
-  testCase: { expected: 1234 },
-  output: 1234,
-  passed: true
-},
-{
-  testCase: { expected: 2345 },
-  output: 23456,
-  passed: false
-},
-{
-  testCase: { expected: "Hello world" },
-  output: "Hola mundo",
-  passed: false
-},
-{
-  testCase: { expected: "Caca" },
-  output: "Caca",
-  passed: true
-},
-{
-  testCase: { expected: "Caca" },
-  output: "Caca",
-  passed: true
-},
-{
-  testCase: { expected: "Caca" },
-  output: "Caca",
-  passed: true
-},
-{
-  testCase: { expected: "Caca" },
-  output: "Caca",
-  passed: true
-},
-{
-  testCase: { expected: "Caca" },
-  output: "Caca",
-  passed: true
-},
-{
-  testCase: { expected: "Caca" },
-  output: "Caca",
-  passed: true
-},
-{
-  testCase: { expected: "Caca" },
-  output: "Caca",
-  passed: true
-},
-{
-  testCase: { expected: "Caca" },
-  output: "Caca",
-  passed: true
-},
-{
-  testCase: { expected: "Caca" },
-  output: "Caca",
-  passed: true
-}]
