42be5c1165c9934bdaf1b1650450df7bf810644e
Alejandro Herrera <alejandro.herrera@mercadolibre.com>
2018-10-20 10:43:21 -0300

    fix de redireccion mediante acciones de redux

--- client/src/middlewares/index.js
+++ client/src/middlewares/index.js
@@ -1,4 +1,6 @@
 import promiseMiddleware from 'redux-promise-middleware';
 import thunk from 'redux-thunk';
+import { routerMiddleware } from 'connected-react-router';
+import history from '../history';
 
-export default [thunk, promiseMiddleware()];
+export default [thunk, promiseMiddleware(), routerMiddleware(history)];
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,12 +1,14 @@
 import { applyMiddleware, createStore } from 'redux';
 import { persistStore } from 'redux-persist';
+import { connectRouter } from 'connected-react-router';
+import history from '../history';
 import reducers from '../reducers';
 import middlewares from '../middlewares';
 
 const storeCreator = applyMiddleware(...middlewares)(createStore);
 
 export const store = storeCreator(
-  reducers /* preloadedState, */,
+  connectRouter(history)(reducers) /* preloadedState, */,
   // eslint-disable-next-line
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
