32715dfe9339dd2c3619df1e24f76ac0e387d70a
Alejandro Herrera <alejandro.herrera@mercadolibre.com>
2018-11-02 23:39:34 -0300

    mapeo de materias, profesores, alumnos y comisiones

---
+++ api/src/src/main/java/org/ungs/gorgory/model/Signature.java
@@ -0,0 +1,38 @@
+package org.ungs.gorgory.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Signature extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    private Long id;
+
+    private String name;
+
+    @OneToMany
+    private List<Course> courses;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+}
