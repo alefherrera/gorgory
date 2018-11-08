d0992cff2a20a9b50863a7f2f9defc273c243917
apujadas <andres.pujadas@mercadolibre.com>
2018-11-07 22:03:13 -0300

    creacion de CourseService para tratar la logica de suscripcion/desuscripcion de los usuarios a los cursos

---
+++ api/src/src/main/java/org/ungs/gorgory/service/impl/CourseService.java
@@ -0,0 +1,46 @@
+package org.ungs.gorgory.service.impl;
+
+import org.springframework.stereotype.Service;
+import org.ungs.gorgory.model.Course;
+import org.ungs.gorgory.model.Roles;
+import org.ungs.gorgory.model.User;
+import org.ungs.gorgory.repository.CourseRepository;
+import org.ungs.gorgory.repository.UserRepository;
+
+@Service
+public class CourseService implements org.ungs.gorgory.service.CourseService {
+
+    private CourseRepository courseRepository;
+    private UserRepository userRepository;
+
+    public CourseService(CourseRepository courseRepository, UserRepository userRepository) {
+        this.courseRepository = courseRepository;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void subscribeUserToCourse(User user, Course course) {
+        if (user.getRole().equals(Roles.STUDENT.getValue())) {
+            course.getStudents().add(user);
+            user.getLearningCourses().add(course);
+        } else if (user.getRole().equals(Roles.TEACHER.getValue())) {
+            course.getTeachers().add(user);
+            user.getTeachingCourses().add(course);
+        }
+        courseRepository.save(course);
+        userRepository.save(user);
+    }
+
+    @Override
+    public void unsubscribeUserToCourse(User user, Course course) {
+        if (user.getRole().equals(Roles.STUDENT.getValue())) {
+            course.getStudents().remove(user);
+            user.getLearningCourses().remove(course);
+        } else if (user.getRole().equals(Roles.TEACHER.getValue())) {
+            course.getTeachers().remove(user);
+            user.getTeachingCourses().remove(course);
+        }
+        courseRepository.save(course);
+        userRepository.save(user);
+    }
+}
