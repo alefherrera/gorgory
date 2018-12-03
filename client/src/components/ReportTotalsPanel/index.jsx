d83a5d710ecc9569c8ce26948687af02d103bf95
apujadas <andres.pujadas@mercadolibre.com>
2018-12-03 01:27:05 -0300

    mejoras visuales

--- client/src/components/ReportTotalsPanel/index.jsx
+++ client/src/components/ReportTotalsPanel/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { SubHeadingText } from '../Generic';
+import { Avatar } from '@material-ui/core';
+import { COLOR_ERROR, COLOR_UNKNOWN } from '../../constants/color';
 
 const Container = styled.div`
   display: flex;
@@ -16,15 +17,14 @@ const InnerContainer = styled.div`
   flex-direction: column;
   width: 100%;
   height: 100%;
-  margin-top: 12px;
+  margin: 6px;
 `;
 
 const Row = styled.div`
-  width: 100%;
   margin-top: 6px;
   margin-bottom: 6px;
   height: ${props => props.height || '100%'};
-  justify-content: ${props => props.align || 'left'};
+  align-self: ${props => props.align || 'left'};
 `;
 
 const ReportTotalsPanel = ({ mostFailedExercise = {}, mostUnknownExercise = {} }) => (
@@ -32,17 +32,23 @@ const ReportTotalsPanel = ({ mostFailedExercise = {}, mostUnknownExercise = {} }
     <InnerContainer>
       <Row align="center">Ejercicio con mas errores</Row>
       <Row align="center">
-        <SubHeadingText
-          text={mostFailedExercise.exercise && mostFailedExercise.exercise.description}
-        />
+        <Avatar style={{ 'background-color': COLOR_ERROR }}>
+          {mostFailedExercise.totals && mostFailedExercise.totals.error}
+        </Avatar>
+      </Row>
+      <Row align="center">
+        {mostFailedExercise.exercise && mostFailedExercise.exercise.description}
       </Row>
     </InnerContainer>
     <InnerContainer>
       <Row align="center">Ejercicio con menos resoluciones</Row>
       <Row align="center">
-        <SubHeadingText
-          text={mostUnknownExercise.exercise && mostUnknownExercise.exercise.description}
-        />
+        <Avatar style={{ 'background-color': COLOR_UNKNOWN }}>
+          {mostUnknownExercise.totals && mostUnknownExercise.totals.error}
+        </Avatar>
+      </Row>
+      <Row align="center">
+        {mostUnknownExercise.exercise && mostUnknownExercise.exercise.description}
       </Row>
     </InnerContainer>
   </Container>
